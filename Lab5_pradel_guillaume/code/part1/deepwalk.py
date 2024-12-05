f610b99570dd1d3fd670b3f15f29cf010c994aae
jddqd <116731767+jddqd@users.noreply.github.com>
2024-12-05 17:52:59 +0100

    Lab 5

---
+++ Lab5_pradel_guillaume/code/part1/deepwalk.py
@@ -0,0 +1,48 @@
+"""
+Deep Learning on Graphs - ALTEGRAD - Nov 2024
+"""
+
+import numpy as np
+import networkx as nx
+from random import choice
+from gensim.models import Word2Vec
+
+
+############## Task 1
+# Simulates a random walk of length "walk_length" starting from node "node"
+def random_walk(G, node, walk_length):
+
+    walk = []
+    for i in range(walk_length):
+        node = choice(list(G.neighbors( node)))
+        walk.append(str(node))
+    
+    return walk
+
+
+
+############## Task 2
+# Runs "num_walks" random walks from each node
+def generate_walks(G, num_walks, walk_length):
+    walks = []
+    
+    for i in range(num_walks):
+      for node in G.nodes:
+        walks.append(random_walk(G, node, walk_length))
+
+    permuted_walks = np.random.permutation(walks)
+
+    return permuted_walks.tolist()
+
+
+# Simulates walks and uses the Skipgram model to learn node representations
+def deepwalk(G, num_walks, walk_length, n_dim):
+    print("Generating walks")
+    walks = generate_walks(G, num_walks, walk_length)
+
+    print("Training word2vec")
+    model = Word2Vec(vector_size=n_dim, window=8, min_count=0, sg=1, workers=8, hs=1)
+    model.build_vocab(walks)
+    model.train(walks, total_examples=model.corpus_count, epochs=5)
+
+    return model
